982899d3b6ae4f0f9ce4b944b090d797e77cb495
novanrohman <novanrohman@gmail.com>
2019-09-19 22:40:57 +0700

    penggunaan variabel pada PHP

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo "Halo semua";
+// Syntax dasar PHP
+// echo "Halo semua";
+
+
+// Penggunana Variabel pada PHP
+$nama = "Novan rohman";
+$kelas = "XII-RPL";
+
+echo $nama;
+echo "</br>";
+echo $kelas;
+echo "</br>";
+echo "hai, perkenalkan nama saya ".$nama." saya dari kelas " .$kelas;
 ?>
